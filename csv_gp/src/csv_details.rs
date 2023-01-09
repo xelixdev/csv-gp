3c2ca577af4fd12d590fa27d6c1a3d5a24d989bc
Robert Hales <rob.hales@xelix.com>
2023-01-09 18:01:22 +0000

    Move formatting to Display trait, pretty print errors

--- csv_gp/src/csv_details.rs
+++ csv_gp/src/csv_details.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fmt::Display};
 
 #[derive(Debug, Clone, Default)]
 pub struct CSVDetails {
@@ -39,5 +39,81 @@ impl CSVDetails {
     pub fn header_messed_up(&self) -> bool {
         let bad_row_count = self.too_few_columns.len() + self.too_many_columns.len();
         self.row_count - 1 == bad_row_count
+    }
+}
+
+impl Display for CSVDetails {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut strings = vec![];
+
+        if self.header_messed_up() {
+            strings.push("The header is totally messed up, no rows have the same number of columns as the header.".to_string());
+        }
+
+        if self.row_count <= 1 {
+            strings.push("There is only one row row in the file.".to_string());
+            return write!(f, "{}", strings.join("\n"));
+        }
+
+        if self.column_count <= 1 {
+            strings.push(format!(
+                "There is {} columns in the file, so the delimiter is almost surely wrong.",
+                self.column_count
+            ));
+            return write!(f, "{}", strings.join("\n"));
+        }
+
+        strings.push(format!(
+            "There are {} rows in the file (including header), with {} columns (according to the header).",
+            self.row_count, self.column_count
+        ));
+
+        if !self.too_few_columns.is_empty() || !self.too_many_columns.is_empty() {
+            strings.push(format!(
+                "There are {} rows with too many columns, and {} rows with too few columns.",
+                self.too_many_columns.len(),
+                self.too_few_columns.len()
+            ));
+        } else {
+            strings.push("All rows have the same number of columns.".to_string());
+        }
+
+        if !self.quoted_delimiter.is_empty() {
+            strings.push(format!(
+                "There are {} lines with correctly quoted delimiter.",
+                self.quoted_delimiter.len()
+            ));
+        } else {
+            strings.push("There are no rows with correctly quoted delimiter.".to_string());
+        }
+
+        if !self.quoted_newline.is_empty() {
+            strings.push(format!(
+                "There are {} lines with correctly quoted newline.",
+                self.quoted_newline.len()
+            ));
+        } else {
+            strings.push("There are no rows with correctly quoted newline.".to_string());
+        }
+
+        if !self.quoted_quote.is_empty() {
+            strings.push(format!(
+                "There are {} lines with correctly quoted quote, out of that {} are absolutely correct.",
+                self.quoted_quote.len(), self.quoted_quote_correctly.len()
+            ));
+        } else {
+            strings.push("There are no rows with correctly quoted quote.".to_string())
+        }
+
+        if !self.incorrect_cell_quote.is_empty() {
+            strings.push(format!(
+                "There are {} lines with incorrect cell quotes.",
+                self.incorrect_cell_quote.len()
+            ));
+        } else {
+            strings.push("There are no rows with incorrect cell quotes.".to_string());
+        }
+
+        write!(f, "{}", strings.join("\n"))
     }
 }
